56af8ea8edf4575976b83165deb6388def47bce7
madhu10kr <madhukiran319@gmail.com>
2018-08-10 12:20:23 +0530

    last

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+
+const mongoose = require('./confg/db');
+const {Ticket} = require('./models/ticket');
+
 const app = express();
 const port = 3000;
-const mongoose = require('./confg/db');
+
 
 //middlewares
 app.use(bodyParser.json());
@@ -15,7 +19,59 @@ app.get('/',(req,res) => {
     res.send({
         msg:'Welcome to ticket master'
     })
-})
+});
+
+app.get('/tickets',(req,res) => {
+    Ticket.find().then((tickets) => {
+        res.send(tickets);
+    })
+    .catch((err) => {
+        res.send(err);
+    })
+});
+
+app.post('/tickets',(req,res) => {
+    let ticket = new Ticket(req.body);
+    ticket.save().then((ticket) => {
+        res.send(ticket);
+    })
+    .catch((err) => {
+        res.send(err);
+    })
+});
+
+app.get('/tickets/:id',(req,res) => {
+    Ticket.findById(req.params.id)
+    .then((ticket) => {
+        res.send(ticket);
+    })
+    .catch((err) => {
+        res.send(err);
+    })
+});
+
+app.put('/tickets/:id',(req,res) => {
+    Ticket.findByIdAndUpdate(req.params.id,req.body)
+    .then((ticket) => {
+        res.send(ticket);
+    })
+    .catch((err) => {
+        res.send(err);
+    })
+});
+
+app.delete('/tickets/:id',(req,res) => {
+    Ticket.findByIdAndRemove(req.params.id)
+    .then((ticket) => {
+        res.send({
+            ticket,
+            msg:'sucessfully deleted'
+        });
+    })
+    .catch((err) => {
+        res.send(err);
+    })
+});
 
 app.listen(port,() => {
     console.log('listening to port ',port);
