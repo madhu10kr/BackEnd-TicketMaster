babb7243b222c350943c90416d052012ae68f0df
madhu10kr <madhukiran319@gmail.com>
2018-08-10 10:08:49 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+//middlewares
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+//route handlers
+
+app.get('/',(req,res) => {
+    res.send({
+        msg:'Welcome to ticket master'
+    })
+})
+
+app.listen(port,() => {
+    console.log('listening to port ',port);
+})
